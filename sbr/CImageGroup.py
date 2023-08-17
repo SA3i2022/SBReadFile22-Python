2345ed5c589b86cc652ce0c8fd2c7adf196ec833
SA3i2022 <107735382+SA3i2022@users.noreply.github.com>
2023-08-17 15:38:49 -0600

    Update CImageGroup.py
    Signed-off-by: SA3i2022 <107735382+SA3i2022@users.noreply.github.com>

--- sbr/CImageGroup.py
+++ sbr/CImageGroup.py
@@ -1,12 +1,12 @@
 __copyright__  = "Copyright (c) 2022, Intelligent Imaging Innovations, Inc. All rights reserved.  All rights reserved."
 __license__  = "This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree."
 
-from BaseDecoder import BaseDecoder
-from CMetadataLib import *
-from CNpyHeader import *
-from CSBFile70 import *
-from CCompressionBase import *
-from CSBPoint import *
+from sbr.BaseDecoder import BaseDecoder
+from sbr.CMetadataLib import *
+from sbr.CNpyHeader import *
+from sbr.CSBFile70 import *
+from sbr.CCompressionBase import *
+from sbr.CSBPoint import *
 import os
 import yaml
 import numpy as np
