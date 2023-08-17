28264dbe0feadeff44484cc325990196e0eeef0c
SA3i2022 <107735382+SA3i2022@users.noreply.github.com>
2023-08-17 15:39:51 -0600

    Update DataLoader.py
    Signed-off-by: SA3i2022 <107735382+SA3i2022@users.noreply.github.com>

--- sbr/DataLoader.py
+++ sbr/DataLoader.py
@@ -1,10 +1,10 @@
 __copyright__  = "Copyright (c) 2022, Intelligent Imaging Innovations, Inc. All rights reserved.  All rights reserved."
 __license__  = "This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree."
 
-from CMetadataLib import *
-from CCompressionBase import *
-from CSBFile70 import *
-from CImageGroup import *
+from sbr.CMetadataLib import *
+from sbr.CCompressionBase import *
+from sbr.CSBFile70 import *
+from sbr.CImageGroup import *
 import numpy as np
 import yaml
 import os.path
