0bb3e7306001f0287abd92e08afbce8d603f90fc
SA3i2022 <107735382+SA3i2022@users.noreply.github.com>
2023-08-16 19:58:38 -0600

    Create setup.py
    Signed-off-by: SA3i2022 <107735382+SA3i2022@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+  
+setup(
+    name='SBReadFile22-Python',
+    version='0.1',
+    description='Slidebook File Reader',
+    author='3i',
+    author_email='na',
+    packages=['SBReadFile22-Python'],
+    install_requires=[
+        'numpy',
+    ],
+)
